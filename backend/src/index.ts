23e0c365a66c5b3133a472d6d2c0c9df55898190
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-01-07 13:44:51 +0200

    Make an endpoint for diagnoses

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,24 @@
 import express from 'express';
 import cors from 'cors';
+import diagnoses from '../data/diagnoses.json'
+
+const diagnosesFromJson = diagnoses 
 
 const app = express();
 
 app.use(cors())
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3002;
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.get('/api/diagnoses', (_req, res) => {
+  res.send(diagnosesFromJson)
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
