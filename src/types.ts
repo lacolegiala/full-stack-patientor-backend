996819c8891bbbf2d699a54f8e2c88d83390797f
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-01-14 11:45:48 +0200

    Add new entry types and patient data with entries

--- src/types.ts
+++ src/types.ts
@@ -11,8 +11,48 @@ export enum Gender {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
 }
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+type SickLeave = {
+  startDate: string;
+  endDate: string;
+};
+
+type Discharge = {
+  date: string;
+  criteria: string;
+};
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+export type Entry = HealthCheckEntry | OccupationalHealthcareEntry | HospitalEntry;
 
 export interface Patient {
   id: string
